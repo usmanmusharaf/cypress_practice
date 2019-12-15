b5a4accc283d1ffe2d52f93ff525c55b98f347cf
Usman Musharaf <usmanmusharaf619@gmail.com>
2019-12-15 21:35:46 +0500

    handled cy.visit callback

--- cypress/integration/insurify_tests/core_flow.spec.js
+++ cypress/integration/insurify_tests/core_flow.spec.js
@@ -1,25 +1,30 @@
 import { HomePage } from "../../pages/homepage"
 import { CarMake } from "../../pages/car_make"
 import { HelperFuncs } from "../../support/helper_funcs"
+import { CarDetail } from "../../pages/car_detail"
 
 describe('Complete happy flow', () => {
 
     // page objects
     const homepage = new HomePage
-    const carmake = new CarMake
+    const carMake = new CarMake
     const helper = new HelperFuncs
-
-    beforeEach(function () {
-        cy.visit('/')
-        helper.requestAssertion('POST', '/api/users/static_user_pageview/')  // asserting user_pageview request
-    })
+    const carDetail = new CarDetail
 
     // core flow of application
     it('Fill in user preferences', () => {
-        cy.title().should('include', 'Insurify®')
-        homepage.enterZipcode('76365')
-        helper.requestAssertion('PATCH', '/api/applicants/application/?product_type=auto') // asserting application patch request
-        carmake.selectCar('2014', 'Honda', 'Civic', 'Hybrid')
+        cy.visit('/', {
+            onLoad: () => {
+                console.log('Page Loading Complete')
+            }
+        }).then(() => {
+            cy.title().should('include', 'Insurify®')
+            homepage.enterZipcode('76365')
+            helper.requestAssertion('PATCH', '/api/applicants/application/?product_type=auto') // asserting application patch request
+            carMake.selectCar('2014', 'Honda', 'Civic', 'Hybrid')
+            carDetail.enterCarDetails('Commute to school', '10', 'Paid in full', 'No Coverage')
+        })
+
     })
 
 })
